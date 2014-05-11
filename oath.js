deca6c91da1dec5c8fa365dae0f12fb501b0fc2e
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-11 16:29:39 -0700

    Added skeleton of oath.js

---
+++ oath.js
@@ -0,0 +1,40 @@
+var oath = {};
+
+(function (exports) {
+  var Promise = function () {
+
+  };
+
+  Promise.prototype.then = function () {
+
+  };
+
+  Promise.prototype.fail = function () {
+
+  };
+
+  var Deferred = function () {
+
+  };
+
+  Deferred.prototype.reject = function(error) {
+    
+  };
+
+  Deferred.prototype.resolve = function(value) {
+    
+  };
+
+  var defer = function () {
+
+  };
+
+  var promisify = function () {
+
+  };
+
+  _.extend(exports, {
+    defer: defer,
+    promisify: promisify
+  });
+}(oath));
