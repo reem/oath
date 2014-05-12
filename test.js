1d36b0f68ae1714de7f1664eabd26058b7c2c141
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-11 18:46:29 -0700

    Added chaining and debugged tests.

--- test.js
+++ test.js
@@ -3,13 +3,13 @@ var assert = chai.assert;
 
 describe('Oath', function () {
   describe('then', function () {
-    it('Should call then on a promise resolution.', function () {
+    it('Should call then on a promise resolution.', function (done) {
       var makePromise = function () {
         var defer = oath.defer();
         setTimeout(function () {
           defer.resolve();
-        }, 50);
-        return defer;
+        }, 5);
+        return defer.promise;
       };
       makePromise()
         .then(done);
@@ -20,25 +20,25 @@ describe('Oath', function () {
         var defer = oath.defer();
         setTimeout(function () {
           defer.resolve(num);
-        }, 50);
-        return defer;
+        }, 5);
+        return defer.promise;
       };
       makePromise(6)
         .then(function (num) {
           expect(num).to.equal(6);
           done();
         });
-    });  
+    });
   });
 
   describe('fail', function () {
-    it('Should call fail on a rejection.', function () {
+    it('Should call fail on a rejection.', function (done) {
       var failingPromise = function () {
         var defer = oath.defer();
         setTimeout(function () {
           defer.reject();
-        }, 50);
-        return defer;
+        }, 5);
+        return defer.promise;
       };
       failingPromise()
         .fail(function () {
@@ -46,13 +46,13 @@ describe('Oath', function () {
         });
     });
 
-    it('Should call fail with the error passed to .reject.', function () {
+    it('Should call fail with the error passed to .reject.', function (done) {
       var failingPromise = function () {
         var defer = oath.defer();
         setTimeout(function () {
           defer.reject("Oh no!");
-        }, 50);
-        return defer;
+        }, 5);
+        return defer.promise;
       };
       failingPromise()
         .fail(function (err) {
@@ -62,7 +62,7 @@ describe('Oath', function () {
     });
   });
 
-  it('Should promisifiy functions with node-style callbacks.', function () {
+  describe('promisify', function () {
     var bigEnough = 100;
     var tooSmall = 10;
     var nodeStyle = function (num, callback) {
@@ -76,76 +76,78 @@ describe('Oath', function () {
     };
 
     var promised = oath.promisify(nodeStyle);
-    describe("promised", function () {
-      it('Should call then on success.', function (done) {
-        promised(bigEnough)
-          .then(function (message) {
-            expect(message).to.equal("That's a big number!");
-            done();
-          });
-      });
+    it('Should call then on success.', function (done) {
+      promised(bigEnough)
+        .then(function (message) {
+          expect(message).to.equal("That's a big number!");
+          done();
+        });
+    });
 
-      it('Should call fail on error.', function (done) {
-        promised(tooSmall)
-          .then(function (message) {
-            expect(message).to.equal("Not big enough!");
-          });
-      });
+    it('Should call fail on error.', function (done) {
+      promised(tooSmall)
+        .fail(function (message) {
+          expect(message).to.equal("Not big enough!");
+          done();
+        });
     });
   });
 
-  it('Should allow you to chain promises using then.', function (done) {
-    var step1 = function (num) {
-      var defer = oath.defer();
-      setTimeout(function () {
-        defer.resolve(num + 10);
-      }, 50);
-      return defer.promise;
-    };
+  describe('chaining', function () {
+    it('Should allow you to chain promises using then.', function (done) {
+      var step1 = function (num) {
+        var defer = oath.defer();
+        setTimeout(function () {
+          defer.resolve(num + 10);
+        }, 5);
+        return defer.promise;
+      };
 
-    var step2 = function (num) {
-      var defer = oath.defer();
-      setTimeout(function () {
-        defer.resolve(num + 20);
-      }, 50);
-      return defer.promise;
-    };
+      var step2 = function (num) {
+        var defer = oath.defer();
+        setTimeout(function () {
+          defer.resolve(num + 20);
+        }, 5);
+        defer.myCheck = "Hello";
+        return defer.promise;
+      };
 
-    step1(100).then(step2).then(function (num) {
-      expect(num).to.equal(130);
-      done();
+      step1(100).then(step2).then(function (num) {
+        expect(num).to.equal(130);
+        done();
+      });
     });
-  });
 
-  it('Should jump directly to fail if an error is thrown during chaining.', function (done) {
-    var step1 = function (num) {
-      var defer = oath.defer();
-      setTimeout(function () {
-        defer.resolve(num + 10);
-      }, 50);
-      return defer.promise;
-    };
+    it('Should jump directly to fail if an error is thrown during chaining.', function (done) {
+      var step1 = function (num) {
+        var defer = oath.defer();
+        setTimeout(function () {
+          defer.resolve(num + 10);
+        }, 5);
+        return defer.promise;
+      };
 
-    var failingStep = function (num) {
-      var defer = oath.defer();
-      setTimeout(function () {
-        defer.reject("Oops!");
-      }, 50);
-      return defer.promise;
-    };
+      var failingStep = function (num) {
+        var defer = oath.defer();
+        setTimeout(function () {
+          defer.reject("Oops!");
+        }, 5);
+        return defer.promise;
+      };
 
-    var didItRun = false;
-    var shouldntRun = function (num) {
-      var defer = oath.defer();
-      didItRun = true;
-      setTimeout(_.partial(defer.resolve.bind(defer), num), 50);
-      return defer.promise;
-    };
+      var didItRun = false;
+      var shouldntRun = function (num) {
+        var defer = oath.defer();
+        didItRun = true;
+        setTimeout(_.partial(defer.resolve.bind(defer), num), 5);
+        return defer.promise;
+      };
 
-    step1(100).then(failingStep).then(shouldntRun).fail(function (err) {
-      expect(err).to.equal("Oops!");
-      expect(didItRun).to.equal(false);
-      done();
+      step1(100).then(failingStep).then(shouldntRun).fail(function errorOut(err) {
+        expect(err).to.equal("Oops!");
+        expect(didItRun).to.equal(false);
+        done();
+      });
     });
   });
 });
