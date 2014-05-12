614eee1a27c6a2b676ed759232da2f2c166c851d
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-11 18:50:55 -0700

    Implemented failable and promisify.

--- oath.js
+++ oath.js
@@ -26,11 +26,30 @@ var oath = {};
   };
 
   var defer = function () {
-
+    return new Deferred(new Promise());
   };
 
-  var promisify = function () {
+  var failable = function (nodeStyle, error, success) {
+    nodeStyle(function (err, data) {
+      if (err) {
+        error(err);
+      } else {
+        success(data);
+      }
+    });
+  };
 
+  var promisify = function (nodeStyle, context) {
+    return function () {
+      var args = _.toArray(arguments);
+      var deferred = defer();
+      failable(
+        function (cb) { nodeStyle.apply(context, args.concat([cb])); },
+        deferred.reject,
+        deferred.resolve
+      );
+      return deferred.promise;
+    };
   };
 
   _.extend(exports, {
